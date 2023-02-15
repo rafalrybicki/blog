c54d0d44666d6c67e4b4e0818d9fa169037ae392
Rafał Rybicki <rafarybicki@gmail.com>
2023-02-15 13:28:27 +0100

    Change post view

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Post;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -43,6 +44,8 @@ class PostRepository extends ServiceEntityRepository
     public function createOrderedByNewestQueryBuilder($categoryId): QueryBuilder
     {
         $qb = $this->createQueryBuilder('p')
+            ->andWhere('p.isApproved = :val')
+            ->setParameter('val', true)
             ->orderBy('p.createdAt', 'ASC')
             ->orderBy('p.updatedAt', 'ASC');
 
