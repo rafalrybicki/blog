2032c0289525b30150c69ca1d471e848f7f3cdb3
Rafał Rybicki <rafarybicki@gmail.com>
2023-02-17 19:38:24 +0100

    Change Post controller

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -38,25 +38,6 @@ class PostController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_post_new', methods: ['GET', 'POST'])]
-    public function new(Request $request): Response
-    {
-        $post = new Post();
-        $form = $this->createForm(PostType::class, $post);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->postRepository->save($post, true);
-
-            return $this->redirectToRoute('app_post_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('post/new.html.twig', [
-            'post' => $post,
-            'form' => $form,
-        ]);
-    }
-
     #[Route('/{slug}', name: 'app_post_show', methods: ['GET'])]
     public function show(Post $post): Response
     {
@@ -64,32 +45,4 @@ class PostController extends AbstractController
             'post' => $post,
         ]);
     }
-
-    // #[Route('/{slug}/edit', name: 'app_post_edit', methods: ['GET', 'POST'])]
-    // public function edit(Request $request, Post $post): Response
-    // {
-    //     $form = $this->createForm(PostType::class, $post);
-    //     $form->handleRequest($request);
-
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $this->postRepository->save($post, true);
-
-    //         return $this->redirectToRoute('app_post_index', [], Response::HTTP_SEE_OTHER);
-    //     }
-
-    //     return $this->renderForm('post/edit.html.twig', [
-    //         'post' => $post,
-    //         'form' => $form,
-    //     ]);
-    // }
-
-    // #[Route('/{slug}', name: 'app_post_delete', methods: ['POST'])]
-    // public function delete(Request $request, Post $post): Response
-    // {
-    //     if ($this->isCsrfTokenValid('delete' . $post->getId(), $request->request->get('_token'))) {
-    //         $this->postRepository->remove($post, true);
-    //     }
-
-    //     return $this->redirectToRoute('app_post_index', [], Response::HTTP_SEE_OTHER);
-    // }
 }
--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -4,7 +4,6 @@ namespace App\Repository;
 
 use App\Entity\Post;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
